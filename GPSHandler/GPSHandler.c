ed4a05e565f3411d493fba5e9b7e6c4f8969057a
Kino <vantin2912@gmail.com>
2023-06-23 11:11:59 +0700

    Done GPS Task Reader

---
+++ GPSHandler/GPSHandler.c
@@ -0,0 +1,65 @@
+/*
+ * GPSHandler.c
+ *
+ *  Created on: Jun 22, 2023
+ *      Author: vanti
+ */
+#include "GPSHandler.h"
+#include "string.h"
+#include "cmsis_os.h"
+
+const osThreadAttr_t GPSTask_attributes = {
+  .name = "GPSTask",
+  .stack_size = 700 * 4,
+  .priority = (osPriority_t) osPriorityNormal2,
+};
+
+void GPS_ReadThread(void* Handler)
+{
+	GPS_HandlerStruct* gpsHandler = (GPS_HandlerStruct*) Handler;
+	lwgps_t lwgps_parser;
+	lwgps_init(&lwgps_parser);
+	char rcvBuffer[GPS_RcvBufferSize];
+	int Status;
+	uint16_t rcvLen = 0;
+	while(1)
+	{
+		if(!gpsHandler->readEnable)
+		{
+			osThreadSuspend(gpsHandler->readThread);
+		}
+		Status = UART_OS_Receive_ToIdle(gpsHandler->uart,(uint8_t*) rcvBuffer, &rcvLen, GPS_RcvBufferSize, osWaitForever);
+		if(Status == osOK)
+		{
+			lwgps_process(&lwgps_parser, rcvBuffer, rcvLen);
+			osMutexAcquire(gpsHandler->dataLock, 10);
+			memcpy(&gpsHandler->lwgps, & lwgps_parser, sizeof(lwgps_t));
+			osMutexRelease(gpsHandler->dataLock);
+		}
+	}
+}
+
+int GPS_Init(GPS_HandlerStruct* gps, UART_OS_HandlerStruct* uart, uint32_t UpdateTime)
+{
+	memset(gps, 0, sizeof(GPS_HandlerStruct));
+	gps->dataLock = osMutexNew(NULL);
+	gps->readThread = osThreadNew(GPS_ReadThread, gps, &GPSTask_attributes);
+
+	gps->uart = uart;
+	gps->updateTime = UpdateTime;
+	return HAL_OK;
+}
+int GPS_Start(GPS_HandlerStruct* gps, uint8_t state)
+{
+	gps->readEnable = 1;
+	osThreadResume(gps->readThread);
+	return HAL_OK;
+}
+
+int GPS_GetData(GPS_HandlerStruct* gps, GPS_DataStruct* Data)
+{
+	osMutexAcquire(gps->dataLock, 10);
+	memcpy(Data, &gps->lwgps, sizeof(GPS_DataStruct));
+	osMutexRelease(gps->dataLock);
+	return HAL_OK;
+}
---
+++ GPSHandler/GPSHandler.h
@@ -0,0 +1,34 @@
+/*
+ * GPSHandler.h
+ *
+ *  Created on: Jun 22, 2023
+ *      Author: vanti
+ */
+
+#ifndef GPSHANDLER_GPSHANDLER_H_
+#define GPSHANDLER_GPSHANDLER_H_
+
+#include "GPSHandler.h"
+#include "lwgps/lwgps.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "UARTHandler/UART_Handler.h"
+
+#define GPS_RcvBufferSize	500
+
+typedef struct GPS_HandlerStruct{
+	UART_OS_HandlerStruct* uart;
+	lwgps_t lwgps;
+	uint32_t updateTime;
+
+	osMutexId_t dataLock;
+	osThreadId_t readThread;
+	uint8_t readEnable;
+} GPS_HandlerStruct;
+typedef lwgps_t GPS_DataStruct;
+
+int GPS_Init(GPS_HandlerStruct* gps, UART_OS_HandlerStruct* uart, uint32_t UpdateTime);
+int GPS_Start(GPS_HandlerStruct* gps, uint8_t state);
+int GPS_GetData(GPS_HandlerStruct* gps, GPS_DataStruct* Data);
+void GPS_ReadThread(void* Handler);
+#endif /* GPSHANDLER_GPSHANDLER_H_ */
